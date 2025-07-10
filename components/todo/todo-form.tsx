9b94c3e00b7552d124209277220a8a3ad29da575
jatiwicaksono98 <jatiwicaksono98@gmail.com>
2025-07-10 22:42:39 +0700

    further revert

--- components/todo/todo-form.tsx
+++ components/todo/todo-form.tsx
@@ -39,12 +39,7 @@ export function AddTodoForm({ onSuccess }: { onSuccess?: () => void }) {
 
   return (
     <Form {...form}>
-      <form
-        onSubmit={() => {
-          form.handleSubmit(onSubmit);
-        }}
-        className="space-y-4"
-      >
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
         <FormField
           control={form.control}
           name="content"
